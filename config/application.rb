125a1c94671b682b17e1f16494c1cb4fa8e54933
Allister Antosik <me@allisterantosik.com>
2015-05-14 20:41:49 +0100

    Fixing hound syntax issues

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,8 @@ module CarMileagerApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        resource '*', headers: :any, methods:
+                                          [:get, :post, :options, :delete]
       end
     end
   end
